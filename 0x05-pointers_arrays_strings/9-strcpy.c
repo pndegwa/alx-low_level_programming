bd6b4d929845d34f9fa06b93696e59c3bb57267e
pndegwa <pndegwa78@gmail.com>
2022-07-12 20:09:51 -0700

    string copy fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,4 +2,21 @@
 
 /**
  * _strcpy -> copies the string pointed to by variable
+ * @dest: this is destiny
+ * @src: this is the copier
  *
+ * Return: This return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
