589c52af5dcfb2487b2056004aa999efd4bdeee4
pndegwa <pndegwa78@gmail.com>
2022-07-12 20:01:44 -0700

    string copy task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * _strcpy -> copies the string pointed to by variable
+ *
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void _puts(char *str);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
 
 #endif
